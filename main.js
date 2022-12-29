a8045e31026e1c52185ece07ed43493ed83258e1
AyanSumitMajumdar <87868278+AyanSumitMajumdar@users.noreply.github.com>
2022-12-29 11:47:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function preload() {
+
+}
+
+function setup(){
+   canvas=createCanvas(302,302);
+   canvas.center();
+
+}
+
+function draw(){
+
+}
+
+function takesnapshot(){
+  save('mustache_filter.png')  
+}
